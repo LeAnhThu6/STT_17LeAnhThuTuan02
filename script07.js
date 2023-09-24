a8892dabda76540281c17bbd7f532c6dec202f91
LeAnhThu6 <124645095+LeAnhThu6@users.noreply.github.com>
2023-09-24 10:36:55 +0700

    Thu push 1

--- script07.js
+++ script07.js
@@ -1,14 +1,14 @@
 // Delete a user
-// var url = "http://localhost:8080/api/v1/users";
-var url = 'https://jsonplaceholder.typicode.com/todos';
+var url = "https://650fabb854d18aabfe9a2abd.mockapi.io/api/todos/";
+// var url = 'https://jsonplaceholder.typicode.com/todos';
 var xhr = new XMLHttpRequest();
-xhr.open("DELETE", url + '/12', true);
-xhr.onload = function() {
-    var users = JSON.parse(xhr.responseText);
-    if (xhr.readyState == 4 && xhr.status == "200") {
-        console.table(users);
-    } else {
-        console.error(users);
-    }
-}
+xhr.open("DELETE", url + "/1", true);
+xhr.onload = function () {
+  var users = JSON.parse(xhr.responseText);
+  if (xhr.readyState == 4 && xhr.status == "200") {
+    console.table(users);
+  } else {
+    console.error(users);
+  }
+};
 xhr.send(null);
